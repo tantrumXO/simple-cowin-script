258bfdec76b1f12bb3cf7bab93c5cee4cfdcb700
Tanuj <39689175+tantrumXO@users.noreply.github.com>
2021-05-30 17:03:47 +0530

    Add files via upload

--- cowin.py
+++ cowin.py
@@ -7,17 +7,15 @@ from datetime import date
 state = "9"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 sender = 'this particular cowin api script thing.'
-# specify the receivers' emails below.
-receivers = ['email_id_1', 'email_id_2']
+receivers = ['tanuj17118@iiitd.ac.in', 'pravek17256@iiitd.ac.in', 'ashwin.singh.gill@gmail.com']
 
 def send_email(message):
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.ehlo()
 	server.starttls()
 	server.ehlo()
-	# specify the credentials of the gmail account you'll use to send emails.
-	server.login('email_id', 'password')
-	server.sendmail('email_id', receivers, message)
+	server.login('tanujdabas@gmail.com', 'ohmyfuckinggodlogmeinalready')
+	server.sendmail('tanujdabas@gmail.com', receivers, message)
 	server.close()
 
 while True:
@@ -31,6 +29,11 @@ while True:
 		id = districts['districts'][i]['district_id']
 		list_d.append(str(id))
 
+	list_d.append(str(199))
+	list_d.append(str(188))
+	list_d.append(str(650))
+	#print(list_d)
+
 	for district in list_d:
 		appointments_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=%s&date=%s" % (district, today)
 		data = requests.get(url = appointments_url, headers = headers).json()
@@ -40,14 +43,14 @@ while True:
 
 				#filters
 		
-				#if data['centers'][i]['sessions'][j]['available_capacity_dose2'] == 0:
-					#match = 0
-
-				if data['centers'][i]['sessions'][j]['available_capacity'] == 0:
+				if data['centers'][i]['sessions'][j]['available_capacity_dose2'] == 0:
 					match = 0
 
-				#if data['centers'][i]['sessions'][j]['vaccine'] != 'COVAXIN':
+				#if data['centers'][i]['sessions'][j]['available_capacity'] == 0:
 					#match = 0
+
+				if data['centers'][i]['sessions'][j]['vaccine'] != 'COVAXIN':
+					match = 0
 
 				if data['centers'][i]['sessions'][j]['min_age_limit'] != 18:
 					match = 0
@@ -61,4 +64,8 @@ while True:
 		send_email(json.dumps(success, indent = 4))
 
 	print(json.dumps(success, indent = 4))
-	time.sleep(300)
+	time.sleep(60)
+
+#print(data['centers'][0]['sessions'][0].keys())
+#print(json.dumps(data, indent=4))
+#time.sleep(seconds)
